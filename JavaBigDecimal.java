d26848aeadc51ad4f7e15cbff239bb7c18345dfb
SilverDragon12 <72190816+SilverDragon12@users.noreply.github.com>
2022-10-12 09:58:46 +0530

    Update JavaBigDecimal.java

--- JavaBigDecimal.java
+++ JavaBigDecimal.java
@@ -5,7 +5,7 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
-public class Solution {
+public class JavaBigDecimal {
    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
