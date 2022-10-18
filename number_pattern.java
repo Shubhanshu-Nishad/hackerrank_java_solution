9dfadb4a6227635ce9dcb45c5f7bb4109bbd2f3e
diraval <116052520+diraval@users.noreply.github.com>
2022-10-18 09:39:12 +0530

    Created number_pattern.java
    A program that prints number in a unique way

---
+++ number_pattern.java
@@ -0,0 +1,17 @@
+//Code by DIRAval
+
+public class number_pattern {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++)
+        {
+            int k=1;
+            for(int j=0;j<=i;j++)
+            {
+                
+                System.out.print(k+"\t");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
