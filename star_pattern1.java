3c7d3734ba2ad5e2da5c404f059389af49a18b0b
diraval <116052520+diraval@users.noreply.github.com>
2022-10-18 09:28:55 +0530

    Created star_pattern1.java
    A java program to print a star program is created

---
+++ star_pattern1.java
@@ -0,0 +1,13 @@
+//Code by DIraval
+class star_pattern1 {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
