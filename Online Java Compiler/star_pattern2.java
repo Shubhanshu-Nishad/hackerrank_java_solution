0827292f6a28076f7e2c7d5c02a777ed58785c26
diraval <116052520+diraval@users.noreply.github.com>
2022-10-18 09:44:55 +0530

    Created star_pattern2.java
    A program that prints star pattern in a unique way

---
+++ Online Java Compiler/star_pattern2.java
@@ -0,0 +1,17 @@
+
+public class star_pattern2 {
+    public static void main(String[] args) {
+        
+        for(int i=5;i>=0;i--)
+        {
+            
+            for(int j=i;j>=0;j--)
+            {
+                
+                System.out.print("*\t");
+              
+            }
+            System.out.println();
+        }
+    }
+}
